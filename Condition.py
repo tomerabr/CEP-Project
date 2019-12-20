ca7db25accec1f6497980ab6c46dc6f874556be2
tomerabr <50576836+tomerabr@users.noreply.github.com>
2019-12-20 12:52:45 +0200

    Add files via upload

--- Condition.py
+++ Condition.py
@@ -22,7 +22,7 @@ class Operand(Enum):
 
 
 class Literal:
-    def __init__(self, event_name_A, event_name_B, operand_A, operand_B, operator):
+    def __init__(self, operator, event_name_A, operand_A, event_name_B, operand_B=None):
         self.event_name_A = event_name_A  # first event name, string
         self.event_name_B = event_name_B  # second event name, string
         self.operand_A = operand_A  # first event operand, string
@@ -45,13 +45,13 @@ class Literal:
         else:
             return False
 
-    def OperandOfStockB(self, stock):
+    def OperandOfStockB(self, stock=None):
+        if stock is None:
+            return self.event_name_B
         if self.operand_B == Operand.OPENING_PRICE:
             return stock.opening
-
         elif self.operand_B == Operand.PEAK_PRICE:
             return stock.peak
-
         elif self.operand_B == Operand.LOWEST_PRICE:
             return stock.lowest
         elif self.operand_B == Operand.CLOSE_PRICE:
@@ -59,9 +59,11 @@ class Literal:
         elif self.operand_B == Operand.VOLUME:
             return stock.volume
 
+    # if stock2 = None it will return the const value
 
-    def checkLiteral(self, stock1, stock2):
-        if stock1.ticker != self.event_name_A or stock2.ticker != self.event_name_B:
+
+    def checkLiteral(self, stock1, stock2=None):
+        if stock1.ticker != self.event_name_A or (stock2 is not None and stock2.ticker != self.event_name_B):
             return False
         if self.operand_A == Operand.OPENING_PRICE:
             return self.checkOperator(stock1.opening, self.OperandOfStockB(stock2))
@@ -85,13 +87,21 @@ class Literal:
         print(self.operator, end='')
         print(" ", end='')
         print(self.event_name_B, end='')
-        print(".", end='')
-        print(self.operand_B, end='')
+        if self.operand_B is not None:
+            print(".", end='')
+            print(self.operand_B, end='')
         print(")", end='')
+
 
     def returnLiteral(self):
         str = self.printLiteral
+
         return str
+
+
+def isUnary(self):
+    return self.operand_B is None
+
 
 # clause is a disjunction of literals, where each literal is a PrimitiveCondition
 # every inner node in the tree is a clause
@@ -99,10 +109,14 @@ class Clause:
     def __init__(self, clause):
         self.clause = clause
 
-    def checkClause(self, stock1, stock2):
+    def checkClause(self, stock1, stock2=None):
         for literal in self.clause:
-            if literal.checkLiteral(stock1, stock2):
-                return True
+            if stock2 == None and literal.isUnary():  # we want to check only Unary
+                if literal.checkLiteral(stock1):
+                    return True
+            elif stock2 is not None and literal.isUnary() == False:
+                if literal.checkLiteral(stock1, stock2):
+                    return True
         return False
 
     def eventsAppearInClause(self):
@@ -111,8 +125,9 @@ class Clause:
         for literal in self.clause:
             if literal.event_name_A not in events:
                 events.append(literal.event_name_A)
-            if literal.event_name_B not in events:
-                events.append(literal.event_name_B)
+            if not literal.isUnary():
+                if literal.event_name_B not in events:
+                    events.append(literal.event_name_B)
 
         return events
 
@@ -121,7 +136,10 @@ class Clause:
         flag = 0
         for literal in self.clause:
             literal.printLiteral()
-            if (flag ==1):
+            if (flag == 1):
                 print(" OR ", end='')
             flag = 1
         print(")", end='')
+
+
+
